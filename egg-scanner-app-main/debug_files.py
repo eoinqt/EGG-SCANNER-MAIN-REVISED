2a818950df8b277747183630568060b6e4b06f28
John Christian Llup <153885857+eoinqt@users.noreply.github.com>
2025-12-13 14:55:33 +0800

    Delete egg-scanner-app-main directory

--- egg-scanner-app-main/debug_files.py
+++
@@ -1,7 +0,0 @@
-import os
-print('Current directory:', os.getcwd())
-print('Directory writable:', os.access('.', os.W_OK))
-print('Files in directory:')
-for f in os.listdir('.'):
-    if f.endswith(('.jpg', '.h5')):
-        print(f'  {f}: exists={os.path.exists(f)}, readable={os.access(f, os.R_OK)}')
